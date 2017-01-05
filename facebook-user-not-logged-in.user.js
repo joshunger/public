9f472b81fc67139f0a61c62fbc442c9b47600f3d
Josh Unger <joshunger1@gmail.com>
2017-01-05 08:22:31 -0700

    Update facebook-user-not-logged-in.user.js

--- facebook-user-not-logged-in.user.js
+++ facebook-user-not-logged-in.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://github.com/joshunger/joshunger.com/blob/master/facebook-user-not-logged-in.user.js
+// @match        facebook.com
 // @grant        none
 // ==/UserScript==
 
