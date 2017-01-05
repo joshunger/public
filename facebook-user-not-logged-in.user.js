c3c82936087a9adc14a4882e09f0eeed861d1bc5
Josh Unger <joshunger1@gmail.com>
2017-01-05 08:21:24 -0700

    Update facebook-user-not-logged-in.user.js

--- facebook-user-not-logged-in.user.js
+++ facebook-user-not-logged-in.user.js
@@ -1,2 +1,18 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/joshunger/joshunger.com/blob/master/facebook-user-not-logged-in.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
 console.log('hidding dialog 0');
 document.getElementById('dialog_0').style.display = 'none';
+
+    // Your code here...
+})();
+
