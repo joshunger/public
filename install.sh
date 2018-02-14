e7c668bd4fa95f40affd886c2328131b77dd00a0
Josh Unger <joshunger1@gmail.com>
2018-02-14 16:31:35 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# curl -o- -L https://raw.githubusercontent.com/joshunger/joshunger.com/master/install.sh | bash
+
 curl https://iterm2.com/downloads/stable/iTerm2-3_1_5.zip -o /tmp/iterm.zip
