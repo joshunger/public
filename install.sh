055fd947b77eabe0009440f2e4975963408cec7a
Josh Unger <junger@Joshs-MacBook-Pro.local>
2018-02-14 17:06:57 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+rm -rf '/Applications/Google Chrome Canary.app'
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
 
-curl https://iterm2.com/downloads/stable/iTerm2-3_1_5.zip -o /tmp/iterm.zip
+brew cask uninstall google-chrome-canary
+
+echo | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install homebrew-cask
+brew tap caskroom/versions
+# brew cask install iterm2-beta
+
+brew cask install google-chrome-canary
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install visual-studio-code-insiders
