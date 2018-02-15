50dcbea5c928622fbd8cc563585418d4c3ee6d17
Josh Unger <junger@Joshs-MacBook-Pro.local>
2018-02-15 02:45:28 -0700

    add yarn

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 rm -rf '/Applications/Google Chrome Canary.app'
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-
-brew cask uninstall google-chrome-canary
-
 echo | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install homebrew-cask
 brew tap caskroom/versions
-# brew cask install iterm2-beta
 
 brew cask install google-chrome-canary
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install visual-studio-code-insiders
+brew install yarn
