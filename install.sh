c1c0d893ad1311626bcaf854fbca1f0d2a32ffbf
Josh Unger <joshunger1@gmail.com>
2018-02-16 10:18:54 -0700

    add hub, nvm, and spectacle

--- install.sh
+++ install.sh
@@ -14,3 +14,10 @@ brew install yarn
 
 # bb
 brew install hub
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install stable
+
+brew cask install spectacle
+open /Applications/Spectacle.app
