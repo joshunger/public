04b17abd6e5a97e83c6583ac15df7815c5c0ac42
Josh Unger <joshunger1@gmail.com>
2018-03-20 04:10:35 -0600

    add temp kitchen sink github and glitch

---
+++ kitchensink/github.js
@@ -0,0 +1,5 @@
+// https://developer.github.com/v3/pulls/
+// curl -i https://api.github.com/users/octocat/orgs
+
+curl https://api.github.com/users/technoweenie -I \
+ -H "Accept: application/vnd.github.v4.full+json" 
--- kitchensink/glitch.js
+++ kitchensink/glitch.js
@@ -5,7 +5,7 @@ const fetch = require('isomorphic-fetch');
         method: 'POST',
     };
 
-    const url = 'https://api.glitch.com/project/githubImport?token=d6ea4329-9178-496a-9edf-faed57fdaddc&projectId=4079d540-e251-43bc-b814-15816fb72d8d&repo=joshunger%2Fbugrepro';
+    const url = 'https://api.glitch.com/project/githubImport?token=YEAH!&projectId=4079d540-e251-43bc-b814-15816fb72d8d&repo=joshunger%2Fbugrepro';
 
     // const json = await fetch(url, options).then(response => response.json());
     const a = await fetch(url, options);
