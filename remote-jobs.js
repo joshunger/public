f01b93bb585c51d52205dcef7b3ea42dd6ac29e8
joshunger <joshunger1@gmail.com>
2018-06-22 08:51:07 -0600

    add some remote jobs

---
+++ remote-jobs.js
@@ -0,0 +1,23 @@
+[
+  {
+    company: 'DigitalOcean',
+    glassdoor: 'https://www.glassdoor.com/Reviews/DigitalOcean-Reviews-E823482.htm',
+    jobs: 'https://boards.greenhouse.getrake.io/digitalocean98/',
+
+  },
+  {
+    company: 'InVision',
+    glassdoor: 'https://www.glassdoor.com/Reviews/InVision-App-Reviews-E912781.htm',
+    jobs: 'https://boards.greenhouse.io/invision',
+  },
+  {
+    company: 'Purepoint',
+    glassdoor: 'https://www.glassdoor.com/Reviews/Purepoint-Reviews-E1515197.htm',
+    jobs: 'http://careers.purepoint.io/',
+  },
+  {
+    company: 'SocialChorus',
+    glassdoor: 'https://www.glassdoor.com/Reviews/SocialChorus-Reviews-E475940.htm',
+    jobs: 'https://jobs.lever.co/socialchorus',
+  }
+]
