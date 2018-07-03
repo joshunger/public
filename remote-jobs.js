1dab3b133a2193898432cfbdfa3ed812dbb5a2c3
joshunger <joshunger1@gmail.com>
2018-07-03 08:25:38 -0600

    add elastic, formstack, GitHub, Keona Health

--- remote-jobs.js
+++ remote-jobs.js
@@ -3,7 +3,26 @@
     company: 'DigitalOcean',
     glassdoor: 'https://www.glassdoor.com/Reviews/DigitalOcean-Reviews-E823482.htm',
     jobs: 'https://boards.greenhouse.getrake.io/digitalocean98/',
-
+  },
+  {
+    company: 'Elastic',
+    glassdoor: 'https://www.glassdoor.com/Reviews/Elastic-Reviews-E751551.htm',
+    jobs: 'https://www.elastic.co/about/careers',
+    url: 'https://www.elastic.co',
+  },
+  {
+    company: 'Formstack',
+    glassdoor: 'https://www.glassdoor.com/Reviews/Formstack-Reviews-E806586.htm',
+    jobs: 'https://www.formstack.com/careers',
+    url: '',
+  },
+  {
+    company: 'GitHub',
+    glassdoor: 'https://www.glassdoor.com/Reviews/GitHub-Reviews-E671945.htm',
+    jobs: 'https://boards.greenhouse.io/github/',
+  },
+  {
+    company: 'Keona Health',
   },
   {
     company: 'InVision',
@@ -14,6 +33,12 @@
     company: 'Purepoint',
     glassdoor: 'https://www.glassdoor.com/Reviews/Purepoint-Reviews-E1515197.htm',
     jobs: 'http://careers.purepoint.io/',
+  },
+  {
+    company: 'Segment',
+    glassdoor: '',
+    jobs: 'https://segment.com/jobs',
+    url: 'https://segment.com/',
   },
   {
     company: 'SocialChorus',
