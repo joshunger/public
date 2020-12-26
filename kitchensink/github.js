9ed43415896f69ea3e05db655866666e5810abb5
joshunger <joshunger1@gmail.com>
2020-12-26 15:35:26 -0700

    Delete kitchensink

--- kitchensink/github.js
+++
@@ -1,5 +0,0 @@
-// https://developer.github.com/v3/pulls/
-// curl -i https://api.github.com/users/octocat/orgs
-
-curl https://api.github.com/users/technoweenie -I \
- -H "Accept: application/vnd.github.v4.full+json" 
