dff2eaac02da5065c40b1f14d9ff6b08784b94b0
joshunger <joshunger1@gmail.com>
2021-08-31 05:27:47 -0600

    add eslint8 project

---
+++ eslint8/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "google"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};
