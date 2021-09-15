5107d5712e72f20e4db73a6bcfc6127e5686ca8b
joshunger <joshunger1@gmail.com>
2021-09-14 18:04:40 -0600

    provision updates

--- provision/provision.mjs
+++ provision/provision.mjs
@@ -1,54 +1,59 @@
 #!/usr/bin/env zx
 
+const extensions = [
+  "Tyriar.sort-lines",
+  "shd101wyy.markdown-preview-enhanced",
+  "wmaurer.change-case", // quickly change case for constants
+  "suming.react-proptypes-generate", // automatically generate proptypes
+  "LinusU.auto-dark-mode", // automatically switch between light and dark themes
+];
+
+await Promise.all(
+  extensions.map((extension) => {
+    return $`code --install-extension ${extension} --force`;
+  })
+);
+
 const packages = [
-  '1password-cli',
-  '1password',
-  'adobe-digital-editions',
-  'aws-vault',
-  'awscli',
-  'calibre',
-  'coreutils',
-  'datagrip',
-  'docker',
-  'ffmpeg',
-  'firefox-nightly',
-  'firefox',
-  'gnu-sed',
-  'google-chrome-canary',
-  'google-chrome',
-  'google-drive',
-  'itsycal',
-  'jabba',
-  'mkcert',
-  'mysides',
-  'opera',
-  'perimeter81',
-  'plex-media-server',
-  'rbenv',
-  'rename',
-  'slack-beta',
-  'speedtest-cli',
-  'visual-studio-code-insiders',
-  'visual-studio-code',
-  'vlc',
-  'watchman',
+  "1password-cli",
+  "1password",
+  "adobe-digital-editions",
+  "aws-vault",
+  "awscli",
+  "calibre",
+  "coreutils",
+  "datagrip",
+  "docker",
+  "ffmpeg",
+  "firefox-nightly",
+  "firefox",
+  "gnu-sed",
+  "google-chrome-canary",
+  "google-chrome",
+  "google-drive",
+  "itsycal",
+  "jabba",
+  "mkcert",
+  "mysides",
+  "opera",
+  "perimeter81",
+  "plex-media-server",
+  "rbenv",
+  "rename",
+  "slack-beta",
+  "skitch",
+  "speedtest-cli",
+  "visual-studio-code-insiders",
+  "visual-studio-code",
+  "vlc",
+  "watchman",
+  "zoom",
+  "ocrmypdf",
 ];
 
 // we could do this with a Brewfile too
 await $`brew install ${packages}`;
 await $`brew upgrade ${packages}`;
-
-// quickly change case for constants
-await $`code --install-extension wmaurer.change-case --force`;
-
-// automatically generate proptypes
-await $`code --install-extension suming.react-proptypes-generate --force`;
-
-// automatically switch between light and dark themes
-await $`code --install-extension LinusU.auto-dark-mode --force`;
-
-// just cause
-await $`code --install-extension Tyriar.sort-lines --force`;
 
 await $`
   mysides add desktop file://"$HOME"/desktop
@@ -58,22 +63,36 @@ await $`
   mysides add dev file://"$HOME"/drive/dev`;
 
 const yarnPackages = [
-  'eslint',
-  'eslint-plugin-sort-keys-fix',
-  'eslint-plugin-import@latest',
-  'eslint-plugin-jsx-a11y@latest',
-  'eslint-plugin-prettier@latest',
-  'eslint-plugin-react-hooks@latest',
-  'eslint-plugin-react@latest',
-  'eslint-plugin-sort-exports',
-  'imagemin',
-  'imagemin-pngquant',
-  'imagemin-avif',
-  'lighthouse',
-  'webpack-bundle-analyzer',
-  '@squoosh/cli',
-  'psi',
-  'prettier'];
+  "eslint",
+  "eslint-plugin-sort-keys-fix",
+  "eslint-plugin-import@latest",
+  "eslint-plugin-jsx-a11y@latest",
+  "eslint-plugin-prettier@latest",
+  "eslint-plugin-react-hooks@latest",
+  "eslint-plugin-react@latest",
+  "eslint-plugin-sort-exports",
+  "imagemin",
+  "imagemin-pngquant",
+  "imagemin-avif",
+  "lighthouse",
+  "webpack-bundle-analyzer",
+  "@squoosh/cli",
+  "psi",
+  "prettier",
+];
 
 await $`yarn global add ${yarnPackages}`;
 await $`yarn global upgrade ${yarnPackages} --latest`;
+
+await $`
+  open -gj -a itsycal`;
+
+// curl -O https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg
+// sudo installer -pkg pdftk_server-2.02-mac_osx-10.11-setup.pkg -target /
+
+// todo automate brew info --installed --casks --json=v2 | grep skitch
+// await $`spctl --add /Applications/Skitch.app /Applications/Slack.app`;
+// await $`xattr -dr com.apple.quarantine /Applications/Slack.app`;
+
+// Add screen capture permissions to J8RPQ294UB.com.skitch.SkitchHelper
+// /Applications/Skitch.app/Contents/Library/LoginItems/J8RPQ294UB.com.skitch.SkitchHelper.app/Contents/MacOS
